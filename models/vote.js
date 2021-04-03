27cf945f442a95179511d90cca8b3c09238d82bc
jimmypark44 <71073823+jimmypark44@users.noreply.github.com>
2021-04-03 15:00:03 +0900

    Update vote.js

--- models/vote.js
+++ models/vote.js
@@ -1,3 +1,13 @@
 const mongoose = require("mongoose");
 
-const voteSchema = new mongoose.Schema()
+const voteSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    select: [asda], // array
+    user: mongoose.ObjectId,
+    comment: [],
+    createAt: Date,
+    updateAt: Date
+
+
+})
