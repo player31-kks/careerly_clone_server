87e695f6ba8f31059496424662bf03453fad0334
kks <keumks789@naver.com>
2021-04-03 14:32:51 +0900

    add User schema

--- models/comment.js
+++ models/comment.js
@@ -1,1 +1,28 @@
-//comments
+const { Schema, model, Types } = require("mongoose")
+
+const UserSchema = new Schema(
+    {
+        name: { type: String, required: true },
+        email: { type: String, required: true },
+        password: { type: String, required: true },
+        role: { type: String, required: true },
+        interest: { type: [String] },
+        introduce: { type: String },
+        career: {
+            type: [{ company: { type: String }, role: { type: String } }],
+        },
+        education: {
+            type: [{ school: { type: String }, major: { type: String } }],
+        },
+        usrImg: { type: String },
+        follower: { type: [{ type: Types.ObjectId, ref: "user" }] },
+        following: { type: [{ type: Types.ObjectId, ref: "user" }] },
+        phone: { type: String, required: true },
+    },
+    {
+        timestamps: true,
+    }
+)
+
+const User = model("user", UserSchema)
+module.exports = User
