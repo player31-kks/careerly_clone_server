0f0edc8350c17947939ec6554d6c5e05a866db62
kks <keumks789@naver.com>
2021-04-03 16:03:10 +0900

    Update Comment Schema

--- models/comment.js
+++ models/comment.js
@@ -1,12 +1,14 @@
 const mongoose = require("mongoose")
 
-const CommentSchema = new mongoose.Schema({
-    cotent: { type: String, required: true },
-    // category:
-    user: { type: Types.ObjectId, required: true, ref: "User" },
-    createAt: Date,
-    updateAt: Date,
-})
+const CommentSchema = new mongoose.Schema(
+    {
+        cotent: { type: String, required: true },
+        user: { type: Types.ObjectId, required: true, ref: "User" },
+    },
+    {
+        timestamps: true,
+    }
+)
 
 const Comment = model("Comment", CommentSchema)
 module.exports = Comment
