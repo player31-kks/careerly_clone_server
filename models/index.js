0b536374c45d3ca6cc55f0c056156ae54d8a50f9
kks <keumks789@naver.com>
2021-04-05 11:33:19 +0900

    Fix models index.js

--- models/index.js
+++ models/index.js
@@ -1,28 +1,4 @@
-const fs = require("fs")
-const { model } = require("mongoose")
-const path = require("path")
-const basename = path.basename(__filename)
 const DB = {}
-
-// const capitalize = (s) => {
-//     if (typeof s !== "string") return ""
-//     return s.charAt(0).toUpperCase() + s.slice(1, s.length - 1)
-// }
-
-// fs.readdirSync(__dirname)
-//     .filter((file) => {
-//         return (
-//             file.indexOf(".") !== 0 &&
-//             file !== basename &&
-//             file.slice(-3) === ".js"
-//         )
-//     })
-//     .forEach((file) => {
-//         const model = require(path.join(__dirname, file))
-//         const modelName = model.collection.collectionName
-//         DB[capitalize(modelName)] = model
-//     })
-
 DB.Post = require("./post")
 DB.Comment = require("./comment")
 DB.User = require("./user")
