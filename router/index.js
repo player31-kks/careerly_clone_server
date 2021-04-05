6c85f0806aed0adebe055a27986ce06b9aa4c277
kks <keumks789@naver.com>
2021-04-05 11:32:58 +0900

    Fix router index.js

--- router/index.js
+++ router/index.js
@@ -1,16 +1,5 @@
-const fs = require("fs")
-const path = require("path")
-const basename = path.basename(__filename)
 const { Router } = require("express")
 const router = Router()
-
-// fs.readdirSync(__dirname)
-//     .filter((folder) => {
-//         return folder !== basename
-//     })
-//     .forEach((folder) => {
-//         router.use("/", require(`./${folder}`))
-//     })
 
 router.use("/", require("./Member"))
 router.use("/post", require("./Post"))
