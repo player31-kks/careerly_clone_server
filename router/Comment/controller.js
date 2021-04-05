fc771609ab8da5e66dce24fbb8c51b45ac70ac3f
jimmypark44 <71073823+jimmypark44@users.noreply.github.com>
2021-04-05 10:53:52 +0900

    fix

--- router/Comment/controller.js
+++ router/Comment/controller.js
@@ -3,13 +3,15 @@ const { isValidObjectId } = require("mongoose")
 
 exports.postComment = async (req, res, next) => {
     const userId = res.locals.user._id
-    const { content, category } = req.body
+    const { postId } = req.params
+    const { content } = req.body
     if (!content) {
         return res.status(400).send({ err: "코멘트가 비었습니다." })
     }
     var NewComment = new Comment({
         content,
-        user: userId
+        user: userId,
+        post: postId
     })
     await NewComment.save()
     return res.send({ success: true })
@@ -21,5 +23,7 @@ exports.deleteComment = async (req, res, next) => {
     return res.send({ success: true })
 }
 exports.getComment = async (req, res, next) => {
+    const { postId } = req.params
+
     return res.send({ success: true })
 }
