4b58aeb0eb498659b12d5175b4c625cf83474f50
kks <keumks789@naver.com>
2021-04-08 23:02:33 +0900

    fix Post and Vote controller

--- router/Vote/controller.js
+++ router/Vote/controller.js
@@ -1,12 +1,12 @@
 const { Vote } = require("../../models")
+const userSelect = ["name", "role", "userImg"]
 
 exports.creatVote = async (req, res, next) => {
   const userId = res.locals.user
-  const { title, description, selection } = req.body
+  const { title, description } = req.body
 
   if (typeof title !== "string")
     return res.status(400).send({ err: "제목이 형식에 맞지 않습니다." })
-
   if (typeof description !== "string")
     return res.status(400).send({ err: "내용이 형식에 맞지 않습니다." })
 
@@ -21,18 +21,21 @@ exports.creatVote = async (req, res, next) => {
 }
 
 exports.getVote = async (req, res, next) => {
-  const userSelect = ["name", "role", "userImg"]
-  const vote = await Vote.find({})
+  try{
+    const vote = await Vote.find({})
     .populate([{ path: "user", select: userSelect }])
     .sort({ updateAt: -1 })
     .limit(17)
   return res.send({ result: vote })
+  }catch(err){
+    console.log(err)
+    return res.status(400).send({ err: err.meassage })
+  }
 }
 
 exports.getVoteDetail = async (req, res, next) => {
   const { voteId } = req.params
   try {
-    const userSelect = ["name", "role", "userImg"]
     const vote = await Vote.findById(voteId).populate([
       { path: "user", select: userSelect },
       { path: "comment", populate: { path: "user", select: userSelect } },
